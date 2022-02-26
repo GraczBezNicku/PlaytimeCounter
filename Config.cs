b2a1cd8ba209db3fdf19d5a9ce3ab8c89ece4b03
GraczBezNicku <58118380+GraczBezNicku@users.noreply.github.com>
2022-02-26 16:22:59 +0100

    Add files via upload

---
+++ Config.cs
@@ -0,0 +1,23 @@
+﻿using Exiled.API.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaytimeCounter
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+
+        [Description("List of groups to log playtime of.")]
+        public List<string> groupsToLog { get; set; } = new List<string>();
+
+        public string webhookURL { get; set; } = "";
+
+        [Description("Message that will display on discord. (can use {seconds} and {hours} too)")]
+        public string webhookMessage { get; set; } = "{time} **{player}** left the server after playing for **{minutes}** minutes";
+    }
+}
---
+++ EventsHandler.cs
@@ -0,0 +1,31 @@
+﻿using Exiled.Events.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaytimeCounter
+{
+    public class EventsHandler
+    {
+        public void OnVerified(VerifiedEventArgs ev)
+        {
+            if(!Plugin.instance.joinTime.ContainsKey(ev.Player.RawUserId) && Plugin.instance.Config.groupsToLog.Contains(ev.Player.GroupName))
+            {
+                Plugin.instance.joinTime.Add(ev.Player.RawUserId, DateTimeOffset.Now.ToUnixTimeSeconds());
+            }
+        }
+
+        public void OnLeft(LeftEventArgs ev)
+        {
+            if(Plugin.instance.joinTime.ContainsKey(ev.Player.RawUserId))
+            {
+                long timeNow = DateTimeOffset.Now.ToUnixTimeSeconds();
+                long secondsPlayed = timeNow - Plugin.instance.joinTime[ev.Player.RawUserId];
+                API.SendWebhook(ev.Player, secondsPlayed);
+                Plugin.instance.joinTime.Remove(ev.Player.RawUserId);
+            }
+        }
+    }
+}
