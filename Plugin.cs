739249155f36f2a7a85635818c50a46aa377c472
GBN <58118380+GraczBezNicku@users.noreply.github.com>
2023-08-11 13:52:14 +0200

    Initial Plugin.cs setup

---
+++ Plugin.cs
@@ -0,0 +1,52 @@
+﻿using HarmonyLib;
+using PluginAPI.Core.Attributes;
+using PluginAPI.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlaytimeCounter
+{
+    public class Plugin
+    {
+        /*
+        FEATURES LIST:
+        Port previous features.
+        Possible config updater?
+        Seperate folders for multiple groups.
+        New config options (count only when round is started etc.)
+        SummaryTimer based on folders. ^
+        SummaryTimer options: (static (specific IDs), dynamic (specific groups))
+        */
+        public static Plugin Instance { get; private set; }
+        private Harmony _harmony;
+
+        [PluginEntryPoint("Playtime Counter", "3.0.0", "Lets you track playitme of specified groups or people.", "GBN")]
+        public void PluginLoad()
+        {
+            Instance = this;
+            EventManager.RegisterAllEvents(this);
+
+            _harmony = new Harmony($"GBN-PLAYTIMECOUNTER-{DateTime.Now}");
+            _harmony.PatchAll();
+        }
+
+        [PluginReload]
+        public void PluginReload()
+        {
+            //Reload all configs here.
+        }
+
+        [PluginUnload]
+        public void PluginUnload()
+        {
+            _harmony.UnpatchAll();
+            _harmony = null;
+
+            EventManager.UnregisterAllEvents(this);
+            Instance = null;
+        }
+    }
+}
